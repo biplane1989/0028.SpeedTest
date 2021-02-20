9a0d6fc79ae1a3b001d814a8ac89b8ea1da790fa
taihoang0895 <48268462+taihoang0895@users.noreply.github.com>
2021-02-20 10:07:50 +0700

    taih fix full screen android

--- app/src/main/java/com/tapi/a0028speedtest/TestActivity.kt
+++ app/src/main/java/com/tapi/a0028speedtest/TestActivity.kt
@@ -2,6 +2,7 @@ package com.tapi.a0028speedtest
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +11,9 @@ import androidx.appcompat.app.AppCompatActivity
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
         setContentView(R.layout.activity_test_setting)
 
     }
+
+
 }
